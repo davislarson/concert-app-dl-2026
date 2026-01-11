630e6c57b80a7935fa190f4c2230e3ea5b84763d
davislarson <davissl@byu.edu>
2026-01-11 19:28:20 +0000

    Added ejs and template page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let express = require("express");
 let app = new express();                                                                     
+app.set("view engine","ejs")
 
 // set up database connection
 const knex = require("knex")({
@@ -21,11 +22,7 @@ app.get("/",(req,res) => {
  .from("venues")
  .then((result) => {
  let html = "<body><ul>";
- for (let i=0;i<result.length;i++) {
-  html += "<li>" + result[i].location + "</li>";
- }
-  html += "</body>"
-  res.send(html);
+ res.render("index", {aConcerts: result});
  }); 
 });
 app.listen(3000);
